f3505fd3569e11c79b5340fe1869f4ad729526ed
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2025-04-27 17:27:42 +0530

    feat: prettier tailwind css

--- src/components/AnimateButton.tsx
+++ src/components/AnimateButton.tsx
@@ -1,25 +1,25 @@
-"use client";
-import React from "react";
-import { motion } from "motion/react";
+'use client';
+import React from 'react';
+import { motion } from 'motion/react';
 
 const AnimateButton = () => {
   return (
     <div>
       <motion.button
-        className="group text-neutral-500 px-12 py-4 rounded-lg bg-black shadow-[0px_2px_1px_0px_rgba(255,255,255,.1)_inset, 0px_-1px_2px_0px_rgba(255,255,255,0.1)_inset] relative translate-z-25"
+        className="group shadow-[0px_2px_1px_0px_rgba(255,255,255,.1)_inset, 0px_-1px_2px_0px_rgba(255,255,255,0.1)_inset] relative translate-z-25 rounded-lg bg-black px-12 py-4 text-neutral-500"
         whileHover={{
           rotateX: 25,
           rotateY: 10,
           y: -5,
-          boxShadow: "0px 10px 100px rgba(8,112,184,0.5)",
+          boxShadow: '0px 10px 100px rgba(8,112,184,0.5)',
         }}
         whileTap={{
           y: 0,
         }}
       >
         <span className="group-hover:text-blue-100">Dalpat Rathore</span>
-        <span className="absolute inset-x-0 bottom-px bg-gradient-to-r from transparent via-cyan-500 to-transparent h-px w-3/4 mx-auto"></span>
-        <span className="opacity-0 group-hover:opacity-100 transition duration-300 absolute inset-x-0 bottom-0 bg-gradient-to-r from transparent via-cyan-500 to-transparent h-1 w-full mx-auto blur-sm"></span>
+        <span className="absolute inset-x-0 bottom-px mx-auto h-px w-3/4 bg-gradient-to-r from-transparent via-cyan-500 to-transparent"></span>
+        <span className="absolute inset-x-0 bottom-0 mx-auto h-1 w-full bg-gradient-to-r from-transparent via-cyan-500 to-transparent opacity-0 blur-sm transition duration-300 group-hover:opacity-100"></span>
       </motion.button>
     </div>
   );
--- src/components/AnimatedCard.tsx
+++ src/components/AnimatedCard.tsx
@@ -46,9 +46,9 @@ const AnimatedCard = () => {
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   className="lucide lucide-cpu-icon lucide-cpu"
                 >
                   <path d="M12 20v2" />

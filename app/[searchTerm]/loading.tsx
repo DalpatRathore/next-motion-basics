2f116c80b9aaaafd8372c7adb955aeb7276c9cf4
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2025-04-27 10:31:11 +0530

    chore: delete old project

--- app/[searchTerm]/loading.tsx
+++
@@ -1,8 +0,0 @@
-const Loading = () => {
-  return (
-    <main className=" mx-auto max-w-lg p-1 min-h-screen flex items-center justify-center">
-      <h2 className="m-4 text-2xl font-bold">Loading...</h2>
-    </main>
-  );
-};
-export default Loading;
